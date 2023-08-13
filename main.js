c14b4e356905a3f7f87451c043672c21c2abfe5d
Subrata Saha <saha.cse.pstu@gmail.com>
2023-08-13 11:54:53 +0600

    final

---
+++ main.js
@@ -0,0 +1,24 @@
+const API_KEY = `28d0ef4ef45070245f1f9a557df3dba2`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+        
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
